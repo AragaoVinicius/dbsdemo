8be87d294bb58010e8c7a5ca2a266192d5a7a552
AragaoVinicius <36004058+AragaoVinicius@users.noreply.github.com>
2018-01-31 16:01:22 +0000

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ echo "Test!";
 <p>Content inside a template tag is hidden from the client.</p>
 
   <h2>DBS</h2>
-  <img src="https://www.nationalgeographic.com/content/dam/science/photos/000/065/6594.ngsversion.1509199314694.adapt.1900.1.jpg">
+  <img src="https://www.nationalgeographic.com/content/dam/science/photos/000/065/6594.ngsversion.1509199314694.adapt.1900.1.jpg" width="200" height="40">
 
 
 <p>A later example will show you how to use JavaScript to display the template content.</p>
