f010b8457f7b4cd29f7c69b33b89efb633a4afdd
AragaoVinicius <36004058+AragaoVinicius@users.noreply.github.com>
2018-01-31 15:54:57 +0000

    Update index.php

--- index.php
+++ index.php
@@ -15,8 +15,8 @@ echo "Test!";
 <p>Content inside a template tag is hidden from the client.</p>
 
 <template>
-  <h2>Flower</h2>
-  <img src="https://orig00.deviantart.net/d29a/f/2016/118/a/f/dbs_kaioken_10x_blue_wallpaper_champa_arc___1_by_windyechoes-da0mehx.jpg" width="214" height="204">
+  <h2>DBS</h2>
+  <img src="https://orig00.deviantart.net/d29a/f/2016/118/a/f/dbs_kaioken_10x_blue_wallpaper_champa_arc___1_by_windyechoes-da0mehx.jpg">
 </template>
 
 <p>A later example will show you how to use JavaScript to display the template content.</p>
